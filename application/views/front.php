65301dc8d8fc6939eae7e123b942961ebc885b61
Andrew [RTech] <in0mad91@gmail.com>
2017-05-26 19:47:57 +0300

    Configured basic output of front page of system

---
+++ application/views/front.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+	<meta charset="utf-8"/>
+	<title>Empiro-index</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,700|Ubuntu:400,700&amp;subset=cyrillic" rel="stylesheet"> 
+	<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>style/style.css" />
+</head>
+<body>
+	<header>
+		<div class="header">
+			<div class="logo"><a href="index.html">Empiro</a></div>
+		</div>
+	</header>
+	<nav>
+		<ul class="nav">
+			<li><a href="admin.html">Для адміністратора</a></li>
+			<li><a href="contact.html">Про нас</a></li>
+		</ul>
+	</nav>
+	<main>
+		<div class="home">
+			<h1>Тестування знань</h1>
+			<p>Ви знаходитесь на головній сторінці тестування знань. Тут ви можете обрати предмет, за яким хочете скласти іспит.</p>
+			<div class="table">
+				<table>
+					<tr>
+						<th>Назва тесту</th>
+						<th>Кількість запитаннь</th>
+						<th>Загальний час проходження</th>
+						<th>Категорія</th>
+						<th>Описання тесту</th>
+						<th>Пройти тест</th>
+					</tr>
+					<tr>
+						<td>HTML/CSS</td>
+						<td>20</td>
+						<td>00:20:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+					<tr>
+						<td>PHP</td>
+						<td>25</td>
+						<td>00:40:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+					<tr>
+						<td>Програмування</td>
+						<td>50</td>
+						<td>00:30:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+					<tr>
+						<td>HTML/CSS</td>
+						<td>20</td>
+						<td>00:20:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+					<tr>
+						<td>PHP</td>
+						<td>25</td>
+						<td>00:40:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+					<tr>
+						<td>Програмування</td>
+						<td>50</td>
+						<td>00:30:00</td>
+						<td>Інформатика</td>
+						<td>Блаблаблаблаблаблабла</td>
+						<td><a href="#">Розпочати</a></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div class="footer">
+			<div class="copyright">
+				<p>&#9400; Copyright 2017 <br /> Школа інформатики ПДМ. Усі права збережені</p>
+			</div>
+			<div class="contact">
+				<p>Рівненський міський Палац дітей та молоді <br /> Адреса <br /> вул. Кн. Володимира, 10, Рівне</p>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
