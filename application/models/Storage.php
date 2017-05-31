1312a6949fe813369d53cb7f85058eba5f4af911
Andrew [RTech] <in0mad91@gmail.com>
2017-05-31 20:08:29 +0300

    Added collecting of accessable file uris and passing them to collector private method

--- application/models/Storage.php
+++ application/models/Storage.php
@@ -18,6 +18,7 @@ class Storage extends CI_Model {
     if(is_dir($storage_folder)) {
       // read contents of folder and return them as simple array
       $files = scandir($storage_folder);
+      $approved_files_uris = array();
       foreach($files as $file) {
         // omit standard files and folders
         if('.' === $file || '..' === $file || 'readme.txt' === $file) {
@@ -25,12 +26,22 @@ class Storage extends CI_Model {
         }
         $filepath = $storage_folder.$file;
         if(is_file($filepath) && is_readable($filepath) && is_writable($filepath) && 'xml' === pathinfo($filepath, PATHINFO_EXTENSION)) {
-          // TODO: handle with this file
+          array_push($approved_files_uris, $filepath);
         }
       }
+      $this->collect_parsed_tests($approved_files_uris);
     }else {
       show_error('Cannot read tests storage folder!', 500);
     }
+  }
+
+  /**
+    * Method specified to collect parsed and validated tests data
+    * into common public list
+    * @param $file_uris array of absolute xml file uris
+    */
+  private function collect_parsed_tests($file_uris) {
+    //TODO: parse, validate and collect tests
   }
 
 }
