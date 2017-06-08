e2af6db5e7d6c7eaa4cc01e729e2d7ffecab2b0c
Andrew [RTech] <in0mad91@gmail.com>
2017-06-08 22:22:31 +0300

    Deep refactor of Storage.php model

--- application/classes/Controller/Main.php
+++ application/classes/Controller/Main.php
@@ -3,25 +3,12 @@
 class Controller_Main extends Controller {
 
 	public function action_index()
-	{	
+	{
+		$tests = array();
+		$storage = Model_Storage::getInstance();
+		$tests = $storage->getTests();
 		$view = new View('front');
-		
-		try{
-			$storage = new Model_Storage();
-			$tests = $storage->check_folder();
-		}catch(StorageAccessException $e) {
-			echo 'Cannot read folder with tests!';
-		}catch(WrongQuestionParametersException $e) {
-			echo 'Question parameters are wrong or do not exist!';
-		}catch(WrongTestParametersException $e) {
-			echo 'Test parameters are wrong or do not exist!';
-		}finally {
-			if(!isset($tests) || empty($tests)) {
-				$tests = array();
-			}
-			$view->tests = $tests;
-		}
-
+		$view->tests = $tests;
 		$this->response->body($view->render());
 	}
 
--- application/classes/Model/Storage.php
+++ application/classes/Model/Storage.php
@@ -1,93 +1,250 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Model_Storage extends Model {
-	
-	/**
+  
+  /**
+   * Used to hold all addresses to test xml files in file system.
+   * @var array $file_uris
+   */
+  public $file_uris;
+  
+  /**
+   * Used to hold all parsed and ready to use tests.
+   * @var array $parsed_tests
+   */
+  private $parsed_tests;
+  
+  /**
+   * Used to hold only one instance of object
+   * according to Singleton pattern.
+   * @var Storage $instance
+   */
+  private static $instance = null;
+  
+  /**
+   * Used when was broken common data in test file.
+   * Format: List of physical absolute addresses to broken file.
+   * @var array $innaccessible_files 
+   */
+  private $innaccessible_files;
+  
+  /**
+   * Used when was broken additional data in test file
+   * such as question or nested answer information.
+   * Format: List of arrays with two params - test name and physical absolute address to broken file
+   * @var array $broken_files
+   */
+  private $broken_files;
+  
+  /**
+   * Used for constant physical address to test files storage
+   * @var const STORAGE_FOLDER
+   */
+  const STORAGE_FOLDER = APPPATH.'test_storage'.DIRECTORY_SEPARATOR;
+  
+  private function __construct() {
+    $this->file_uris = array();
+    $this->innaccessible_files = array();
+    $this->broken_files = array();
+    $this->parsed_tests = array();
+  }
+  
+  public function __destruct() {
+    $this->file_uris = null;
+    $this->innaccessible_files = null;
+    $this->broken_files = null;
+  }
+  
+  /**
+   * Get singleton instance of Storage object.
+   * @return Storage instance
+   */
+  public static function getInstance() {
+    if(self::$instance === null){
+        self::$instance = new self; 
+    }
+    return self::$instance;
+  }
+  
+  /**
     * Checks storage folder and it contents for accessibility
+    * @return bool true or false according to validation result
     */
-  public function check_folder() {
-    $storage_folder = APPPATH.'test_storage'.DIRECTORY_SEPARATOR;
-    if(is_dir($storage_folder)) {
-      // read contents of folder and return them as simple array
-      $files = scandir($storage_folder);
-      $approved_files_uris = array();
-      foreach($files as $file) {
-        // omit standard files and folders
-        if('.' === $file || '..' === $file || 'readme.txt' === $file) {
-          continue;
-        }
-        $filepath = $storage_folder.$file;
-        if(is_file($filepath) && is_readable($filepath) && is_writable($filepath) && 'xml' === pathinfo($filepath, PATHINFO_EXTENSION)) {
-          array_push($approved_files_uris, $filepath);
+  private function checkStorageFolderAccessibility() {
+    try {
+      if(is_dir(self::STORAGE_FOLDER)) {
+        return true;
+      }else {
+        throw new StorageAccessException('Cannot read tests storage folder', 500);
+      }
+    }catch(StorageAccessException $e) {
+      //TODO: add error message handling.
+    }
+  }
+  
+  /**
+   * Read all test files with .xml extenstion and collect them
+   * for future processing
+   */
+  private function collectTestsInStorage() {
+    // read contents of folder and return them as simple array
+    $files = scandir(self::STORAGE_FOLDER);
+    foreach($files as $file) {
+      // omit standard files and folders
+      if('.' === $file || '..' === $file || 'readme.txt' === $file) {
+        continue;
+      }
+      $filepath = self::STORAGE_FOLDER.$file;
+      if(is_file($filepath) && is_readable($filepath) && is_writable($filepath) && 'xml' === pathinfo($filepath, PATHINFO_EXTENSION)) {
+        array_push($this->file_uris, $filepath);
+      }
+    }
+  }
+  
+  /**
+   * Validate collected files with tests
+   * for special rules that every tests must match
+   * @throws WrontTestParametersException
+   * @throws WrongQuestionParametersException
+   */
+  private function validateCollectedTests() {
+    if(isset($this->file_uris) && !empty($this->file_uris) && is_array($this->file_uris)) {
+      foreach($this->file_uris as $key => $uri) {
+        $parsed = simplexml_load_file($uri);
+        try{
+          if(!self::checkMainTestParams($parsed)){
+            throw new WrongTestParametersException();
+          }
+          foreach($parsed->questions->question as $question) {
+            if(self::checkQuestion($question)) {
+              foreach($question->answers->answer as $answer) {
+                if(!self::checkAnswer($answer)) {
+                  throw new WrongQuestionParametersException();
+                }
+              }
+            }else {
+              throw new WrongQuestionParametersException();
+            }
+          }
+        }catch(WrongTestParametersException $e) {
+          // if we catch this exception
+          // it means that common test data is broken
+          // so we suspect that the test title
+          // also is not available
+          array_push($this->innaccessible_files, $uri);
+          unset($this->file_uris[$key]);
+        }catch(WrongQuestionParametersException $e) {
+          // if we catch this exception
+          // it means that common test data is fine
+          // but additional information as questions or answers
+          // are broken and cannot be read
+          $broken_file_info = new stdClass;
+          $broken_file_info->name = $parsed->name;
+          $broken_file_info->filepath = $uri;
+          array_push($this->broken_files, $broken_file_info);
+          unset($broken_file_info);
+          unset($this->file_uris[$key]);
         }
       }
-      return $this->collect_parsed_tests($approved_files_uris);
-    }else {
-			throw new StorageAccessException('Cannot read tests storage folder', 500);
     }
+  }
+  
+  /**
+   * Parse and return all accessible tests for future using.
+   * @return array List of parsed tests or empty array()
+   */
+  public function getTests() {
+    if($this->checkStorageFolderAccessibility()) {
+      $this->collectTestsInStorage();
+      $this->validateCollectedTests();
+      $this->parseCollectedTests();
+      if(!empty($this->parsed_tests) && count($this->parsed_tests) > 0) {
+        return $this->parsed_tests;
+      }
+      return array();
+    }
+  }
+  
+  /**
+   * Check main parameters of test for accessibility
+   * @var $parsed object with parsed xml document
+   * return bool true or false value
+   */
+  public static function checkMainTestParams(&$parsed) {
+    return
+      isset(
+        $parsed->name,
+        $parsed->category,
+        $parsed->time,
+        $parsed->allowtaskreviews,
+        $parsed->allowtoreanswer
+      )
+      && !empty((string)$parsed->name)
+      && !empty((string)$parsed->category)
+      && !empty((string)$parsed->time)
+      && preg_match("/^[0-9]{2}:[0-9]{2}$/", $parsed->time)
+      && !empty((string)$parsed->allowtoreanswer)
+      && !empty((string)$parsed->allowtoreanswer);
+  }
+  
+  /**
+   * Check question for non-emptiness and additional attributes.
+   * @param reference $question Link to question object
+   * return bool result of validation
+   */
+  public static function checkQuestion(&$question) {
+    $question_attr = $question->answers->attributes();
+    return
+        isset($question->title)
+        && !empty((string)$question->title)
+        && !empty((string)$question_attr['type']);
+  }
+  
+  /**
+   * Check answer for non-emptiness and additional attributes.
+   * @param reference $answer Link to answer object
+   * return bool result of validation
+   */
+  public static function checkAnswer(&$answer) {
+    $answer_attr = current($answer->attributes());
+    return isset($answer_attr['is_right']) && strlen(trim((string)$answer)) && !empty((string)$answer_attr['is_right']);
   }
 
   /**
     * Method specified to collect parsed and validated tests data
     * into common public list
-    * @param $file_uris array of absolute xml file uris
     */
-  private function collect_parsed_tests($file_uris) {
-    foreach($file_uris as $uri) {
+  private function parseCollectedTests() {
+    if(!isset($this->file_uris) || empty($this->file_uris) || !is_array($this->file_uris)) {
+      return array();
+    }
+    foreach($this->file_uris as $uri) {
       $parsed = simplexml_load_file($uri);
       $all_tests = array();
       $test = new stdClass;
-      if(!isset($parsed->name) || empty($parsed->name)){
-        throw new WrongTestParametersException();
-      }
       $test->name = (string)$parsed->name;
-      if(!isset($parsed->category) || empty($parsed->category)){
-        throw new WrongTestParametersException();
-      }
       $test->category = (string)$parsed->category;
-      if(!isset($parsed->time) || empty($parsed->time) || !preg_match("/^[0-9]{2}:[0-9]{2}$/", $parsed->time)){
-        throw new WrongTestParametersException();
-      }
       $test->time = (string)$parsed->time;
-      if(!isset($parsed->allowtaskreviews) || empty($parsed->allowtaskreviews)){
-        throw new WrongTestParametersException();
-      }
       $test->allowtaskreviews = (bool)$parsed->allowtaskreviews;
-      if(!isset($parsed->allowtoreanswer) || empty($parsed->allowtoreanswer)){
-        throw new WrongTestParametersException();
-      }
       $test->allowtoreanswer = (bool)$parsed->allowtoreanswer;
       $test->questions = array();
       foreach($parsed->questions->question as $question) {
         $new_question = new stdClass;
         $question_attr = current($question->answers->attributes());
-        if(!isset($question->title) || empty($question->title)){
-          throw new WrongQuestionParametersException();
-        }
         $new_question->title = (string)$question->title;
-        if(!isset($question_attr['type']) || empty($question_attr['type'])){
-          throw new WrongQuestionParametersException();
-        }
         $new_question->type = (bool)$question_attr['type'];
         $new_question->answers = array();
         foreach($question->answers->answer as $answer) {
           $answer_attr = current($answer->attributes());
           $new_answer = new stdClass;
-          if(!isset($answer_attr['is_right']) || empty($answer_attr['is_right'])){
-            throw new WrongQuestionParametersException();
-          }
           $new_answer->is_right = (bool)$answer_attr['is_right'];
-          if(!isset($answer) || empty($answer)){
-            throw new WrongQuestionParametersException();
-          }
-            $new_answer->title = (string)$answer;
-            array_push($new_question->answers, $new_answer);
-          }
-          array_push($test->questions, $new_question);
+          $new_answer->title = (string)$answer;
+          array_push($new_question->answers, $new_answer);
         }
-        array_push($all_tests, $test);
-      }               
-    return $all_tests;
+        array_push($test->questions, $new_question);
+      }
+      array_push($this->parsed_tests, $test);
+    }
   }
 
 } // End Storage
