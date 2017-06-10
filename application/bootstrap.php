06406ae6621abaf739dc96b583c5bae116eac5f6
Andrew [RTech] <in0mad91@gmail.com>
2017-06-10 18:49:35 +0300

    Added route for contacts page

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -145,6 +145,11 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+Route::set('contact_page', 'contacts')
+	->defaults(array(
+		'controller' => 'main',
+		'action' => 'contacts'
+	));
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'main',
