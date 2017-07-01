aafb469666e5ee59a2158e566af39f2a4bf6ee35
Tony <kostia2013ua@gmail.com>
2017-07-01 19:17:10 +0300

    Implemened getting of basic test data with validation

--- application/classes/Controller/Test.php
+++ application/classes/Controller/Test.php
@@ -3,9 +3,19 @@
 class Controller_Test extends Controller {
     public function action_create()
 	{
+        $data = null;
+        if($this->request->method() === Request::POST) {
+            $data = (object)filter_var_array($_POST, FILTER_SANITIZE_STRING);
+            Model_Test::validateInitialData($data);
+        }
+        $messages = MessageHandler::getInstance()->getMessages();
 		$view = new View('layout');
 		$view->header = new View('header');
 		$view->content = new View('test_creation_st1');
+        if(null !== $data) {
+            $view->content->data = $data;
+        }
+        $view->content->messages = $messages;
 		$view->footer = new View('footer');
 		$this->response->body($view->render());
 	}
