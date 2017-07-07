f2b1670d708ad3509e9a2237e78a18db5330c989
Tony <kostia2013ua@gmail.com>
2017-07-07 22:40:04 +0300

    Implemented displaying of tests in table at admin_main.php

--- application/classes/Controller/Admin.php
+++ application/classes/Controller/Admin.php
@@ -1,6 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Admin extends Controller {
+
+  private $tests;
  
   public function action_entrance() {
     if($this->request->method() === Request::POST) {
@@ -22,11 +24,18 @@ class Controller_Admin extends Controller {
   }
   
   public function action_main() {
+    $storage = Model_Storage::getInstance();
+		$this->tests = $storage->getTests();
+    $tests = array();
+    if(null !== $this->tests && count($this->tests) > 0) {
+      $tests = $this->tests;
+    }
     $messages = MessageHandler::getInstance()->getMessages();
     $view = new View('layout');
     $view->header = new View('header');
     $view->content = new View('admin_main');
     $view->content->messages = $messages;
+    $view->content->tests = $tests;
     $view->footer = new View('footer');
     $this->response->body($view->render());
   }
--- application/views/admin_main.php
+++ application/views/admin_main.php
@@ -22,24 +22,27 @@
 			</div>		
 				<h1>Розділ адміністратора</h1>
 				<p>Вам доступні такі операції, як створення та редагування існуючих тестів.</p>
-		
 				<div class="table">
-					<table>
-						<tr>
-							<th>Назва тесту</th>
-							<th>Кількість запитань</th>
-							<th>Час проходження</th>
-							<th>Категорія / Дисципліна</th>
-							<th>Пройти тест</th>
-						</tr>
-						<tr>
-							<td>PHP</td>
-							<td>20</td>
-							<td>00:20:00</td>
-							<td>Інформатика</td>
-							<td><a href="#">Редагувати</a></td>
-						</tr>
-					</table>
+					<?php if(!empty($tests) && is_array($tests) && count($tests) > 0): ?>
+						<table>
+							<tr>
+								<th>Назва тесту</th>
+								<th>Кількість запитань</th>
+								<th>Час проходження</th>
+								<th>Категорія / Дисципліна</th>
+								<th>Редагування</th>
+							</tr>
+							<?php foreach($tests as $test): ?>
+								<tr>
+									<td><?php echo $test->name; ?></td>
+									<td><?php echo count($test->questions); ?></td>
+									<td><?php echo date('H:i:s', strtotime($test->time)); ?></td>
+									<td><?php echo $test->category; ?></td>
+									<td><a href="#">Редагування</a></td>
+								</tr>
+							<?php endforeach; ?>
+						</table>
+						<?php endif; ?>
 				</div>
 		</div>
 	</main>
