25ba3adc4bc4ab47d3f4d4b6e53458093a6a09f9
Tony <kostia2013ua@gmail.com>
2017-07-25 16:03:19 +0300

    Function createAnswerRows was moved to front_api.js

--- application/views/test_creation_st2.php
+++ application/views/test_creation_st2.php
@@ -1,31 +1,5 @@
 <script type="text/javascript">
   $(document).ready(function(){
-    function createAnswerRows(type) {
-        var template = $('#field_template li');
-        var count = 0;
-        if('one' === type) {
-          count = 3;
-        }else if('multiple' === type) {
-          count = 5;
-        }
-        $('#answers_list').html('');
-        for(var i = 1; i <= count; i++) {
-          var newAnswerField = $(template).clone();
-          $(newAnswerField).find('.right-answer input').val(i);
-          $(newAnswerField).appendTo('#answers_list');
-        }
-        if('multiple' === type) {
-          $('#answers_list .right-answer input').each(function(){
-            $(this).attr('type', 'checkbox');
-            $(this).attr('name', 'right_answers[]');
-            $(this).removeClass('radio').addClass('checkbox');
-            $('#answers_list .radio-custom').each(function(){
-            $(this).removeClass('radio-custom').addClass('checkbox-custom');
-          });
-          });
-        }
-        $('#answers_list').show();
-    }
     $('#selector-answer-type').change(function(){
       var value = $('#selector-answer-type').val();
       createAnswerRows(value);
