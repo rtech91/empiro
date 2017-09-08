7527f4f36c27b265988c352bd48e6ccafab57e7d
Andrew [RTech] <in0mad91@gmail.com>
2017-09-08 20:42:08 +0300

    Added basic template for SVG_Diagram class

---
+++ application/classes/SVG/Diagram.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class SVG_Diagram {
+
+  const TYPE_SECTORAL = 1;
+
+  const TYPE_CHART = 2;
+
+  const TYPE_QUADRATIC = 3;
+
+  public function __constructor($type, $width, $height) {
+      
+  }
+
+}
