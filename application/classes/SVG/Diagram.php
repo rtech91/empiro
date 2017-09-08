66f80d93ef1a60e54acda7570c6f3a4b4f42f883
Andrew [RTech] <in0mad91@gmail.com>
2017-09-08 21:15:28 +0300

    Implemented creating of basic layout of svg image

--- application/classes/SVG/Diagram.php
+++ application/classes/SVG/Diagram.php
@@ -7,9 +7,23 @@ class SVG_Diagram {
   const TYPE_CHART = 2;
 
   const TYPE_QUADRATIC = 3;
+  
+  private $markup;
 
-  public function __constructor($type, $width, $height) {
-      
+  public function createBasicTemplate($type, $width, $height) {
+      $this->markup = new stdClass;
+      $this->markup->open = "<svg width=\"$width\" height=\"$height\">";
+      $this->markup->body 	= '';
+      $this->markup->close = "</svg>";
+  }
+  
+  public function save() {
+    $layout = '';
+    $layout .= $this->markup->open;
+    $layout .= $this->markup->body;
+    $layout .= $this->markup->close;
+    
+    return $layout;
   }
 
 }
