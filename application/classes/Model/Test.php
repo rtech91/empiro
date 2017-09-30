28082047784db05a8e2725b979ed3040c36ca899
Tony <kostia2013ua@gmail.com>
2017-09-30 17:40:44 +0300

    Patched validation & messages in Test.php model

--- application/classes/Model/Test.php
+++ application/classes/Model/Test.php
@@ -5,31 +5,31 @@ class Model_Test extends Model {
         $mh = MessageHandler::getInstance();
         $is_correct = true;
         if(!Valid::not_empty($data->name)){
-            $mh->registerMessage('Name is empty!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+            $mh->registerMessage('Name is empty or forbidden symbols detected!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::max_length($data->name, 32)){
             $mh->registerMessage('Max name length is 32 symbols!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
-        if(!empty($data->name) && preg_match('[^a-zA-Z0-9-&-\s]', $data->name)){
-            $mh->registerMessage('Not allowed to use anything but letters, numbers and underscores in name!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+        if(!empty($data->name) && preg_match("[(a-zA-Z0-9-&\s.+)]", $data->name)){
+            $mh->registerMessage('Invalid symbols in name!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::not_empty($data->category)){
-            $mh->registerMessage('Category is empty!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+            $mh->registerMessage('Category is empty or forbidden symbols detected!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::max_length($data->category, 16)){
             $mh->registerMessage('Max category length is 16 symbols!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
-        if(!empty($data->category) && preg_match('[^a-zA-Z0-9-&-\s]', $data->category)){
-            $mh->registerMessage('Not allowed to use anything but letters, numbers and underscores!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+        if(!empty($data->category) && preg_match("[(a-zA-Z0-9-&\s.+)]", $data->category)){
+            $mh->registerMessage('Invalid symbols in category!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::not_empty($data->total_time)){
-            $mh->registerMessage('Total time is empty!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+            $mh->registerMessage('Total time is empty or forbidden symbols detected!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::max_length($data->total_time, 3)){
@@ -41,7 +41,7 @@ class Model_Test extends Model {
             $is_correct = false;
         }
         if(!Valid::not_empty($data->min_right_answers)){
-            $mh->registerMessage('Minimum right answers is empty!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
+            $mh->registerMessage('Minimum right answers is empty or forbidden symbols detected!', (MessageHandler::MH_ERROR | MessageHandler::ACCESS_ADMIN));
             $is_correct = false;
         }
         if(!Valid::max_length($data->min_right_answers, 2)){
