36f1e4cc0b523d5975e4c7ad4be0c6f88256a05e
Andrew [RTech] <in0mad91@gmail.com>
2018-07-06 19:15:08 +0300

    Configured and fixed JSON storing into localStorage. Added simple error message and clearing page contents if test is null or corrupted.

--- application/views/test_pass_st2.php
+++ application/views/test_pass_st2.php
@@ -2,7 +2,6 @@
 var mins = 1;
 var countDownTime = new Date();
 countDownTime.setMinutes(countDownTime.getMinutes() + mins);
-var distance = 0;
 var time = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownTime - now;
@@ -10,13 +9,13 @@ var time = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     if (hours < 10) {
-        var hours = "0" + hours;
+        hours = "0" + hours;
     }
     if (minutes < 10) {
-        var minutes = "0" + minutes;
+        minutes = "0" + minutes;
     }
     if (seconds < 10) {
-        var seconds = "0" + seconds;
+        seconds = "0" + seconds;
     }
     document.getElementById("test-time").innerHTML = hours + ":" + minutes + ":" + seconds;
     mins-=1;
@@ -31,20 +30,22 @@ function stopTimer(){
     localStorage.timeResult = document.getElementById("test-time").innerHTML;
 }
 document.addEventListener('DOMContentLoaded', function(){
-    var parsedTest = JSON.parse('<?php echo $test; ?>');
-    if(parsedTest) {
-        localStorage.setItem('parsedTest', parsedTest);
-        var test = localStorage.getItem('parsedTest');
-        console.log(test);
+    var parsedTest = <?php echo str_replace(array('\n', '\t'), '', $test); ?>;
+    if(null !== parsedTest && undefined !== parsedTest && 'object' === typeof(parsedTest)) {
+        localStorage.setItem('parsedTest', JSON.stringify(parsedTest));
+        var test = JSON.parse(localStorage.getItem('parsedTest'));
     }else {
-        console.error('Cannot parse test data!');
+				clearInterval(time);
+        console.error('Cannot parse test data! Contents: ' + parsedTest);
+				$('h1').html('');
+				$('.question, .test-block, .result-block, .test-info').remove();
+				$('.alert-wrap').html('<div class="alert-failure"><span>Неможливо продовжити тестування! Зверніться до адміністратора.</span></div>');
     }
 });
 </script>
 <main>
 <div class="home">
     <div class="alert-wrap">
-        <div class="alert-failure"><span><?php echo I18n::get('Test HTML/CSS damaged'); ?></span></div>
     </div>
     <h1>HTML/CSS</h1>
     <div class="test-wrap">
